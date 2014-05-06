f411e9addd5db05eab07c0d662bd9fe073a5786c
Cory Diamand <corydiamand@gmail.com>
2014-05-05 21:01:36 -0400

    no authentication

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,6 +1,6 @@
 class GoodsController < ApplicationController
   before_action :set_good, only: [:show, :edit, :update, :destroy]
-  skip_before_filter :authenticate_user!
+  skip_before_filter :verify_authenticity_token
 
   # GET /goods
   # GET /goods.json

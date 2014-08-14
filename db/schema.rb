2066055a9a453895387bb250665e562b902a1702
Cory Diamand <corydiamand@gmail.com>
2014-08-14 17:45:29 -0400

    add has_many relationship to user for goods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814202915) do
+ActiveRecord::Schema.define(version: 20140814214002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,11 @@ ActiveRecord::Schema.define(version: 20140814202915) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
+    t.integer  "user_id"
   end
 
   add_index "goods", ["app_instance_id"], name: "index_goods_on_app_instance_id", using: :btree
+  add_index "goods", ["user_id"], name: "index_goods_on_user_id", using: :btree
 
   create_table "parent_apps", force: true do |t|
     t.text     "name"

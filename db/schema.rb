962e62cd14ad2af3bca586892b660dbb7abe0f02
Jeff <jeffbritton93@gmail.com>
2014-08-14 16:34:08 -0400

    Added the descriptionn column to the goods model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506014925) do
+ActiveRecord::Schema.define(version: 20140814202915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140506014925) do
     t.integer  "app_instance_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "goods", ["app_instance_id"], name: "index_goods_on_app_instance_id", using: :btree

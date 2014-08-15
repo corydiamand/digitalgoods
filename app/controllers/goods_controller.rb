32afbcd5977c7811438f637df05ae9856714204e
Cory Diamand <corydiamand@gmail.com>
2014-08-14 22:48:46 -0400

    allow more params in goods

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -72,6 +72,6 @@ class GoodsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def good_params
-      params.require(:good).permit(:name, :type, :user_id, :parent_app_id, :app_instance_id)
+      params.require(:good).permit(:name, :type, :user_id, :parent_app_id, :app_instance_id, :description)
     end
 end
